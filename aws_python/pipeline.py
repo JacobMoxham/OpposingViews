c3d848c7fec91a68796e5924e764fe18942452e8
Sophie Durrant <sgd38@cam.ac.uk>
2018-02-15 21:06:20 +0000

    Add performance indicators

--- aws_python/pipeline.py
+++ aws_python/pipeline.py
@@ -5,6 +5,7 @@ from similar_articles.frontend import find_similar_articles
 from classifiers.classifiers import classify
 from suitability_scoring.calculate_suitability import get_suitable_articles
 
+import time
 import urllib
 
 def test(event):
@@ -18,13 +19,23 @@ def test(event):
              "summary": "What was the point of banning Russia from the Winter Olympics when 169 of their athletes are still being allowed to compete as neutrals? "}]
 
 def pipeline_test(passed_url):
+    #TODO: Use proper logger
+    start_time = time.time()
+
     # extract content of passed url
     article = extract_content(passed_url)
+    extraction_time = time.time()
+    print("Extracting content from article took " + str(extraction_time - start_time) + " seconds")
+    
     # find similar articles based on keywords
     similar_articles = find_similar_articles(article.meta_keywords)
+    similar_article_time = time.time()
+    print("Finding similar articles took " + str(similar_article_time - extraction_time) + " seconds")
+
     # run heuristics on initial article
     initial_heuristics = classify({'text': article.cleaned_text})
-
+    initial_heuristic_time = time.time()
+    print("Running initial heuristics took " + str(initial_heuristic_time - similar_article_time) + " seconds")
 
     # run heuristics on each similar article
     comparison_heuristics_list = []
@@ -37,9 +48,13 @@ def pipeline_test(passed_url):
         except:
             print('error extracting:', url)
 
+    comparasion_heuristic_time = time.time()
+    print("Running comparison heuristics took " + str(comparasion_heuristic_time - initial_heuristic_time) + " seconds")
+
     # run suitability calculations
     suitable_articles = get_suitable_articles(initial_heuristics, comparison_heuristics_list)[:3]
-
+    suitability_calculation_time = time.time()
+    print("Running suitability calculations took " + str(suitability_calculation_time - comparasion_heuristic_time) + " seconds")
 
     # format for sending to plugin
     articles_to_return = []
--- aws_python/server.py
+++ aws_python/server.py
@@ -2,6 +2,7 @@
 from bottle import Bottle, route, request, run
 import pipeline
 import json
+import time
 
 app = Bottle()
 
@@ -11,10 +12,14 @@ def success():
 
 @app.route('/get-views', method='POST')
 def handle_event():
+    request_time = time.time()
     url = request.forms.get('link')
-    print(url)
     res = pipeline.pipeline_test(url)
-    print(res)
-    return json.dumps(res)
+    json_res = json.dumps(res)
+    response_time = time.time()
+    print("Total time of processing: " +
+            str(response_time - request_time) +
+            " seconds")
+    return json_res
 
 run(app, host='localhost', port='8080')
