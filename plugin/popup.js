fc4a4249c828a58633f83c4db167449476427555
Harry Jones <i.am@harryj.co.uk>
2018-02-16 13:16:27 +0000

    Show errors when plugin can't fetch articles
    Fixes #24

--- plugin/popup.js
+++ plugin/popup.js
@@ -65,36 +65,37 @@ function sendFeedback(feedback, fromLink, suggestedArticleLink, thumbsElem) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true },
         function(tabs) {
             data = '{"from": "' + fromLink + '", "to": "' + suggestedArticleLink + '", "feedback": "' + feedback + '"}';
+            data = {
+                "from": fromLink,
+                "to": suggestedArticleLink,
+                "feedback": feedback
+            };
 
-            sendAPIRequest(data,
-                feedbackProcessingAPI,
-                (res) => {
+            $.post(feedbackProcessingAPI, requestData)
+            .done((res) => {
                     alert(JSON.parse(res).message);
                     thumbsElem.hide();
-                });
+            }).fail((jqXHR, textStatus, errorThrown) => {
+                    $(document.body).text(`Failed to submit feedback: ${textStatus}`);
+            });
         });
 }
 
 
-function onWindowLoad() {
-    var message = document.getElementById('container');
+function onExtensionWindowLoad() {
     $("#suggested-article-list").hide();
     $("#feedback-request").hide();
 
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const currentTab = tabs[0];
+        const currentURL = currentTab.url;
+        chrome.tabs.sendMessage(currentTab.id, {
                 'action': 'sourceRequest',
-                'currURL': tabs[0].url
-            },
-            function(rtn) {
-                data = 'link=' + tabs[0].url;
-
+                'currURL': currentURL
+            }, function(rtn) {
                 if (rtn.prevURL) {
                     $("#feedback-request .thumbs-up").click({
-                        toLink: tabs[0].url,
+                        toLink: currentURL,
                         fromLink: rtn.prevURL,
                         thumbsElem: $("#feedback-request")
                     }, (ev) => {
@@ -102,42 +103,32 @@ function onWindowLoad() {
                     });
 
                     $("#feedback-request .thumbs-down").click({
-                        toLink: tabs[0].url,
+                        toLink: currentURL,
                         fromLink: rtn.prevURL,
                         thumbsElem: $("#feedback-request")
                     }, (ev) => {
                         sendFeedback("negative", ev.data.fromLink, ev.data.toLink, ev.data.thumbsElem);
                     });
-                    
+
                     $("#feedback-request").show();
                 }
 
-                sendAPIRequest(data, backendProcessingAPI, (res) => {
-                    createSuggestedArticleTable(JSON.parse(res), tabs[0].url);
+                const requestData = {
+                    'link': currentURL
+                };
+                $.post(backendProcessingAPI, requestData)
+                .done((res) => {
+                    createSuggestedArticleTable(JSON.parse(res), currentURL);
                     $("#suggested-article-list").show();
+                })
+                .fail((jqXHR, textStatus, errorThrown) => {
+                    $(document.body).text(`Failed to find suggested articles: ${textStatus}`);
                 });
-
             });
     });
 }
 
 
-function sendAPIRequest(data, api, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", api, true);
-    var msg = data;
-
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            callback(xhr.responseText);
-        }
-    };
-
-    xhr.send(msg);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    onWindowLoad();
+    onExtensionWindowLoad();
 });
