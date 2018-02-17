e9924caa08850af8ccddf702a6437ae41e91026f
Jacob Moxham <jacobmoxham@live.co.uk>
2018-02-17 16:41:44 +0000

    Updated content extraction to use generic backends like similar articles, should allow cleaner integration of backends other than goose

--- aws_python/pipeline.py
+++ aws_python/pipeline.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from content_extraction.goose.extract_content import extract_content
+from content_extraction.extract_content import extract_content
 from similar_articles.frontend import find_similar_articles
 from classifiers.classifiers import classify
 from suitability_scoring.calculate_suitability import get_suitable_articles
@@ -28,7 +28,7 @@ def pipeline_test(passed_url):
     extraction_time = time.time()
     print("Extracting content from article took " + str(extraction_time - start_time) + " seconds")
     
-    article_keywords = article.meta_keywords
+    article_keywords = article['keywords']
     print("Keywords: {:s}".format(", ".join(article_keywords)))
 
     # find similar articles based on keywords
@@ -38,7 +38,7 @@ def pipeline_test(passed_url):
     print("Finding similar articles took " + str(similar_article_time - extraction_time) + " seconds")
 
     # run heuristics on initial article
-    initial_heuristics = classify({'text': article.cleaned_text})
+    initial_heuristics = classify({'text': article['text']})
     initial_heuristic_time = time.time()
     print("Running initial heuristics took " + str(initial_heuristic_time - similar_article_time) + " seconds")
 
@@ -48,8 +48,8 @@ def pipeline_test(passed_url):
         try:
             url = entry['url']
             comparison_article = extract_content(url)
-            comparison_heuristics = classify({'text': comparison_article.cleaned_text})
-            comparison_heuristics_list.append(({'article': comparison_article, 'url': url}, comparison_heuristics))
+            comparison_heuristics = classify({'text': comparison_article['text']})
+            comparison_heuristics_list.append((comparison_article, comparison_heuristics))
         except:
             print('error extracting:', url)
 
@@ -58,17 +58,18 @@ def pipeline_test(passed_url):
 
     # run suitability calculations
     suitable_articles = get_suitable_articles(initial_heuristics, comparison_heuristics_list)[:3]
-    print("Suitable articles:\n\t{:s}".format("\n\t".join([a['article'].title for a,_ in suitable_articles])))
+    print("Suitable articles:\n\t{:s}".format("\n\t".join([a['title'] for a,_ in suitable_articles])))
     suitability_calculation_time = time.time()
     print("Running suitability calculations took " + str(suitability_calculation_time - comparasion_heuristic_time) + " seconds")
 
     # format for sending to plugin
     articles_to_return = []
-    for article, _ in suitable_articles:
-        ret_article = {"link": article['url'],
+    for suitable_article, _ in suitable_articles:
+        ret_article = {"link": suitable_article['url'],
                        "imageLink": "",
-                       "title": article['article'].title,
-                       "summary": article['article'].cleaned_text[:100]}
+                       "title": suitable_article['title'],
+                       "summary": '%s%s' % (suitable_article['text'][:100], '...')
+                       }
         articles_to_return.append(ret_article)
 
     # return 3 suitable articles
