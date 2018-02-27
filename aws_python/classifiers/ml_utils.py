9d9682d10b3b2838b19dbe1d54d2bc157c339c0b
Sophie Durrant <sgd38@cam.ac.uk>
2018-02-27 12:29:50 +0000

    Add opinion vs neutral classifier

--- aws_python/classifiers/ml_utils.py
+++ aws_python/classifiers/ml_utils.py
@@ -12,4 +12,6 @@ def show_most_informative_features(vectorizer, clf, n=20):
     coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
     top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
     for (coef_1, fn_1), (coef_2, fn_2) in top:
-        logging.info("\t{}\t{}\t\t{}\t{}".format(coef_1, fn_1, coef_2, fn_2))
+        line = "\t{}\t{}\t\t{}\t{}".format(coef_1, fn_1, coef_2, fn_2)
+        logging.info(line)
+
