f77b729d72c4f687e98f47c28ab0f7b90747d93b
Radu <radu20_04@yahoo.com>
2018-03-01 11:33:36 +0000

    Proper Nouns Extraction

---
+++ aws_python/content_extraction/newspaper/test_proper_nouns.py
@@ -0,0 +1,24 @@
+from extract_proper_nouns import ExtractProperNouns
+from extract_content_newspaper import ExtractContentNewspaper
+
+urls = [
+    "http://www.bbc.co.uk/news/uk-politics-42867668",
+    "http://www.dailymail.co.uk/tvshowbiz/article-5328767/Kim-Kardashian-hits-cultural-appropriation.html",
+    "https://www.theguardian.com/lifeandstyle/2012/sep/07/kim-kardashian-life-as-brand",
+    "https://www.hellomagazine.com/tags/kim-kardashian/",
+    "https://www.vanityfair.com/news/2017/12/donald-trump-wines",
+    "https://www.nbcnews.com/politics/donald-trump/trump-s-gripes-against-mccabe-included-wife-s-politics-comey-n842161"
+]
+
+n = ExtractContentNewspaper()
+pn = ExtractProperNouns()
+
+articles = [n.extract_content(url) for url in urls]
+
+for article in articles:
+    	
+    print (article['title'])
+    print("Title: ", pn.extract_proper_nouns(article['title']))
+    
+    
+print("\n")
