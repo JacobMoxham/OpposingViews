9c2ed2233c3a65ecf0ce2cee32c089f0a492b776
Jacob Moxham <jacobmoxham@live.co.uk>
2018-03-04 22:08:57 +0000

    Uploaded to and tested on new server but with hacks rather than nice https and relative paths

--- plugin/config.js
+++ plugin/config.js
@@ -1,9 +1,9 @@
 const config = {
     DEBUG_URL_BASE : "http://localhost:8080/",
-    PROD_URL_BASE :"https://opposingviews.media/api/",
+    PROD_URL_BASE :"http://159.65.27.201:8080/",
     FEEDBACK_URL_SLUG : "feedback-processing",
     ARTICLE_SUGGESTION_SLUG : "get-views",
     CACHE_TIMEOUT : 1000 * 60 * 60 * 24 * 3,
     IS_DEBUG : 0,
-    DISABLE_CACHING: true
+    DISABLE_CACHING: false
 };
